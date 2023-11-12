f2eba2ee12ba3f1d297c08eebaa069d0a8703944
brasa <lucasfracaro0403@gmail.com>
2023-11-12 15:12:58 -0300

    first commit

---
+++ docker.bat
@@ -0,0 +1,5 @@
+docker run -w /root -it --rm alpine:latest sh -uelic '
+apk add git nodejs neovim ripgrep build-base wget --update
+git clone https://github.com/NvChad/NvChad ~/.config/nvim
+nvim
+''
