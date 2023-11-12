62c58ce3cb2809f8ea237476248230e142ce969d
brasa <88164818+bl4z3y@users.noreply.github.com>
2023-11-12 15:17:46 -0300

    Update docker.bat

--- docker.bat
+++ docker.bat
@@ -2,4 +2,4 @@ docker run -w /root -it --rm alpine:latest sh -uelic '
 apk add git nodejs neovim ripgrep build-base wget --update
 git clone https://github.com/NvChad/NvChad ~/.config/nvim
 nvim
-''
+'
